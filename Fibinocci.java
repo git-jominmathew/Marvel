fb4e8102a65bdd9f318e49f4c70f3658e60ce13a
git-jominmathew <jominazhakathu@gmail.com>
2019-03-27 22:49:48 +0900

    Real Fibinocci program

--- Fibinocci.java
+++ Fibinocci.java
@@ -1,13 +1,17 @@
 import java.lang.*;
 public class Fibinocci{
-int a = 0;
-int b=1;
-int temp;
-sysout(a);sysout(b);
-for(int i=2;i<10;i++){
-temp = b;
-b=a+b;
-sysout(b);
-a=temp;
-change
+    public static void main(String args[]){
+        int a = 0;
+        int b=1;
+        int temp;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=2;i<10;i++){
+            temp = b;
+            b=a+b;
+            System.out.println(b);
+            a=temp;
+    }
+}
+
 }
